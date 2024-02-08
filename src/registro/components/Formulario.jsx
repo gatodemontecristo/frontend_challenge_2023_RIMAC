8f144509fb35183e87dcc60d118516ab426b1bec
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2024-02-08 03:55:39 -0500

    Terminano el formulario con las validaciones

--- src/registro/components/Formulario.jsx
+++ src/registro/components/Formulario.jsx
@@ -1,5 +1,56 @@
+import {  useState } from "react";
 import "../styles/formularioStyle.scss";
 export const Formulario = () => {
+  const [dniValue, setDNIValue] = useState({ documento: "", error: true });
+  const [numberValue, setNumberValue] = useState({ numero: "", error: true });
+  const [typeDocValue, setTypeDocValue] = useState("pasaporte");
+  const [conditionValue1, setConditionValue1] = useState(false);
+  const [conditionValue2, setConditionValue2] = useState(false);
+  const [errorValue, setErrorValue] = useState(false);
+
+  const onDNIChange = (event) => {
+    const result = event.target.value.replace(/\D/g, "");
+    const validation = `${typeDocValue == "dni" ? 8 : 12}`;
+    if (result.length <= validation) {
+      if (parseInt(result).toString().length == validation)
+        setDNIValue({ documento: result, error: false });
+      else setDNIValue({ documento: result, error: true });
+    }
+  };
+  const onNumberChange = (event) => {
+    const result = event.target.value.replace(/\D/g, "");
+    if (result.length <= 10) {
+      if (parseInt(result).toString().length == 10)
+      setNumberValue({ numero: result, error: false });
+      else setNumberValue({ numero: result, error: true });
+    }
+  };
+  const onTypeDocChange = ({ target }) => {
+    setTypeDocValue(target.value);
+    setDNIValue({ documento: "", error: true });
+  };
+
+  const onConditionChange1 = () => {
+    setConditionValue1(!conditionValue1);
+  };
+  const onConditionChange2 = () => {
+    setConditionValue2(!conditionValue2);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (
+      !dniValue.error &&
+      !numberValue.error &&
+      conditionValue1 &&
+      conditionValue2
+    ) {
+      setErrorValue(false);
+    } else {
+      setErrorValue(true);
+    }
+  };
+
   return (
     <div className="formulario">
       <div className="formulario__texto">
@@ -14,69 +65,123 @@ export const Formulario = () => {
           asesoría. 100% online.
         </h3>
       </div>
+      <form className="formulario__registro" onSubmit={onSubmit}>
+        <div className="formulario__ingreso">
+          <div className="input-group">
+            <select
+              className="w-30 form-select"
+              id="floatingSelect"
+              onChange={onTypeDocChange}
+              value={typeDocValue}
+            >
+              <option value="dni">DNI</option>
+              <option value="pasaporte">Pasaporte</option>
+            </select>
+            <div
+              className={`w-70 formulario__ingreso--input form-floating ${
+                errorValue && dniValue.error ? "is-invalid" : ""
+              }`}
+            >
+              <input
+                type="text"
+                className={`form-control ${
+                  errorValue && dniValue.error ? "is-invalid" : ""
+                }`}
+                id="floatingTextarea"
+                placeholder="Username"
+                onChange={onDNIChange}
+                value={dniValue.documento}
+              />
+              <label for="floatingTextarea">Nro. de documento</label>
+            </div>
+            <div className="invalid-feedback">
+              {typeDocValue == "dni"
+                ? "Ingrese un DNI válido (8 dígitos)"
+                : "Ingrese un pasaporte válido (12 dígitos)"}
+            </div>
+          </div>
 
-      <div className="formulario__ingreso">
-        <div className="input-group">
-          <select className="w-30 form-select" id="floatingSelect">
-            <option value="dni" selected>
-              DNI
-            </option>
-            <option value="pasaporte">Pasaporte</option>
-          </select>
-          <div className="w-70 formulario__ingreso--input form-floating">
+          <div
+            className={`form-floating ${
+              errorValue && numberValue.error ? "is-invalid" : ""
+            }`}
+          >
             <input
               type="text"
-              className="form-control"
-              id="floatingTextarea"
-              placeholder="Username"
+              className={`form-control ${
+                errorValue && numberValue.error ? "is-invalid" : ""
+              }`}
+              id="floatingInputGroup3"
+              placeholder="Celular"
+              onChange={onNumberChange}
+              value={numberValue.numero}
             />
-            <label for="floatingTextarea">Nro. de documento</label>
+            <label for="floatingInputGroup3">Celular</label>
+            <div className="invalid-feedback">
+            Ingrese un número de celular válido (10 dígitos)
           </div>
+          </div>
+        
         </div>
 
+        <div className="formulario__condiciones">
+          <div
+            className={`form-check ${
+              errorValue && !conditionValue1 ? "is-invalid" : ""
+            }`}
+          >
+            <input
+              className={`form-check-input ${
+                errorValue && !conditionValue1 ? "is-invalid" : ""
+              }`}
+              type="checkbox"
+              id="flexCheckDefault"
+              checked={conditionValue1}
+              onChange={onConditionChange1}
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Acepto lo Política de Privacidad
+            </label>
+            <div className="invalid-feedback">
+            Debes aceptar la Política de Privacidad para continuar
+          </div>
+          </div>
+        
 
-        <div className="form-floating">
-          <input
-            type="text"
-            class="form-control"
-            id="floatingInputGroup3"
-            placeholder="Celular"
-          />
-          <label for="floatingInputGroup3">Celular</label>
-        </div>
-      </div>
+          <div
+            className={`form-check ${
+              errorValue && !conditionValue2 ? "is-invalid" : ""
+            }`}
+          >
+            <input
+              className={`form-check-input ${
+                errorValue && !conditionValue2 ? "is-invalid" : ""
+              }`}
+              type="checkbox"
+              id="flexCheckDefault"
+              checked={conditionValue2}
+              onChange={onConditionChange2}
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Acepto la Política Comunicaciones Comerciales
+            </label>
+            <div className="invalid-feedback">
+            Debes aceptar la Política Comercial para continuar
+          </div>
+          </div>
+          
 
-      <div className="formulario__condiciones">
-        <div className="form-check">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-          <label class="form-check-label" for="flexCheckDefault">
-            Acepto lo Política de Privacidad
-          </label>
+          <a href="">
+            <p className="formulario__condiciones--term">
+              Aplican Términos y Condiciones.
+            </p>
+          </a>
         </div>
-        <div className="form-check">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-          <label class="form-check-label" for="flexCheckDefault">
-            Acepto la Política Comunicaciones Comerciales
-          </label>
-        </div>
-        <p className="formulario__condiciones--term">
-          Aplican Términos y Condiciones.
-        </p>
-      </div>
 
-      <button type="button" className="formulario__btnFinal btn btn-dark">
-        Cotiza aquí
-      </button>
+        <button type="submit" className="formulario__btnFinal btn btn-dark">
+          Cotiza aquí
+        </button>
+      </form>
     </div>
   );
 };
